d444a55e4341a9e6cfff5f32fd7bd21aabe53c6c
Hiqss <105363317+Hiqss@users.noreply.github.com>
2022-06-24 18:12:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var numberOne = parseInt(prompt ("Enter first number"))
+var numberTwo = parseInt(prompt ("Enter second number"))
+var Operator = prompt ("Enter Operator you want to perform(+, -, *, /)")
+
+  if (Operator == "+")
+   {
+    alert (numberOne + numberTwo)
+  }
+  else if (Operator == "-") 
+  {
+    alert (numberOne - numberTwo)
+  }
+  else if (Operator == "/")
+  {
+    alert (numberOne / numberTwo)
+  }
+  else if (Operator == "*") 
+  {
+    alert (numberOne * numberTwo)
+  }
